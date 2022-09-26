7bc5a7536684b643a3fff7b968d72d48a087de04
xebi14 <xebi.unity@gmail.com>
2022-09-26 15:27:20 +0500

    checkbox updated

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -10,13 +10,13 @@ export const Checkbox = ({ listItems }: CheckboxProps) => {
         {listItems.map((item) => (
           <div className="relative flex items-start">
             <div className="flex h-5 items-center">
-              <input id="comments" aria-describedby="comments-description" name="comments" type="checkbox" className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500" />
+              <input id={item.heading} aria-describedby="comments-description" name="comments" type="checkbox" className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500" />
             </div>
             <div className="ml-3 text-sm">
-              <label htmlFor="comments" className="font-medium text-gray-700">
+              <label htmlFor={item.heading} className="font-medium text-gray-700">
                 {item.heading}
               </label>
-              <span id="comments-description" className="text-gray-500">
+              <span id={`${item.heading}-description`} className="text-gray-500">
                 <span className="sr-only">{listItems[0].heading} </span>
                 {item.description}
               </span>
